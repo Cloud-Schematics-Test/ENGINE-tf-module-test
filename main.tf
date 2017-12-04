3bb155f4b164637faed656fb281deb6ec3151ef3
ruediger maass <ruediger.maass@de.ibm.com>
2017-12-04 13:01:21 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,15 @@
+variable "sample_var" {
+  description = "A sample_var to pass to the template."
+  default     = "hello, hello"
+}
+
+variable "sleepy_time" {
+  description = "How long our local-exec will take a nap."
+  default     = 10
+}
+
 module "mymodule"
   source = "github.com/Cloud-Schematics-Test/ENGINE-tf-cloudless-sleepy"
-  sample_var = "hello, this is a module test"
+  sleepy_time = "${var.sleepy_time}"
+  sample_var = "${var.sample_var}"
 }
